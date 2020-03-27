24d96e1df0460a904e03748481abe774c48d9c21
Lucas Espindola <lucas.espindola@hotmail.com>
2020-03-26 21:40:22 -0300

    check for cors

--- back/app.py
+++ back/app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, jsonify
 from models.algebraicPosition import AlgebraicPosition
 from services.chessService import getValidPositions
@@ -9,4 +10,7 @@ def possible_moves(position):
     if (len(position) != 2) :
         raise ValueError("Only positions in Algebraic Notation are accepted. (e.g. B3)")
     algebraicPosition = AlgebraicPosition(position[0], int(position[1]))
-    return jsonify(list(getValidPositions(algebraicPosition)))
+    response = jsonify(list(getValidPositions(algebraicPosition)))
+    if 'FLASK_ENV' in os.environ and os.environ['FLASK_ENV'] == 'development':
+        response.headers.add('Access-Control-Allow-Origin', '*')
+    return response
